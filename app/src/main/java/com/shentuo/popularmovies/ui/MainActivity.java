2c327700e46396e2e13d9daa05cc06b2506d269b
Shentuo Zhan <shentuo.zhan@tigerspike.com>
2017-04-24 13:56:24 +0800

    replaced asyncTask with taskLoader

--- app/src/main/java/com/shentuo/popularmovies/ui/MainActivity.java
+++ app/src/main/java/com/shentuo/popularmovies/ui/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,26 +15,30 @@ import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.shentuo.popularmovies.R;
 import com.shentuo.popularmovies.global.Constants;
 import com.shentuo.popularmovies.model.Poster;
-import com.shentuo.popularmovies.ui.utilities.AsyncTaskCompleteListener;
-import com.shentuo.popularmovies.ui.utilities.GetMoviePostersTask;
 import com.shentuo.popularmovies.ui.utilities.NetworkUtils;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.net.URL;
 
-public class MainActivity extends AppCompatActivity implements MoviesAdapter.ListItemClickListener {
+public class MainActivity extends AppCompatActivity implements MoviesAdapter.ListItemClickListener, LoaderManager.LoaderCallbacks<String> {
     private MoviesAdapter mAdapter;
     private RecyclerView mMoviesList;
     private final int MOST_POPULAR = 1;
     private final int TOP_RATED = 2;
+    private static final String GET_MOVIE_QUERY_URL = "getMovies";
+    private static final int GET_MOVIE_LOADER = 22;
+    private ProgressBar mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +48,7 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Lis
         setSupportActionBar(toolbar);
 
         mMoviesList = (RecyclerView) findViewById(R.id.rv_movies);
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
         final GridLayoutManager layoutManager = new GridLayoutManager(this, numberOfColumns());
         mMoviesList.setLayoutManager(layoutManager);
 
@@ -74,28 +82,77 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Lis
             requestUrl = NetworkUtils.buildUrlForTopRated();
         }
 
-        new GetMoviePostersTask(new FetchMyDataTaskCompleteListener()).execute(requestUrl);
+        Bundle queryBundle = new Bundle();
+
+        queryBundle.putString(GET_MOVIE_QUERY_URL, requestUrl.toString());
+
+        LoaderManager loaderManager = getSupportLoaderManager();
+        Loader<String> getMovieLoader = loaderManager.getLoader(GET_MOVIE_LOADER);
+
+        if (getMovieLoader == null) {
+            loaderManager.initLoader(GET_MOVIE_LOADER, queryBundle, this);
+        } else {
+            loaderManager.restartLoader(GET_MOVIE_LOADER, queryBundle, this);
+        }
     }
 
-    private class FetchMyDataTaskCompleteListener implements AsyncTaskCompleteListener<String> {
-        @Override
-        public void onTaskComplete(String getMoviesResults) {
-            if (getMoviesResults != null && !getMoviesResults.equals("")) {
-                try {
-                    JSONObject response = new JSONObject(getMoviesResults);
-                    JSONArray results = response.getJSONArray(Constants.RESULT_KEY);
-                    mAdapter.clearItems();
-                    for (int i = 0; i < results.length(); i++) {
-                        mAdapter.addPosterItem(new Poster(results.getJSONObject(i)));
-                    }
-                    mAdapter.notifyDataSetChanged();
-                } catch (JSONException e) {
-                    e.printStackTrace();
+    @Override
+    public Loader<String> onCreateLoader(int id, final Bundle args) {
+        return new AsyncTaskLoader<String>(this) {
+            @Override
+            protected void onStartLoading() {
+
+                if (args == null) {
+                    return;
                 }
+
+                mLoadingIndicator.setVisibility(View.VISIBLE);
+
+                forceLoad();
+            }
+
+            @Override
+            public String loadInBackground() {
+                String getMovieUrlString = args.getString(GET_MOVIE_QUERY_URL);
+
+                if (getMovieUrlString == null) {
+                    return null;
+                }
+
+                try {
+                    URL getMovieUrl = new URL(getMovieUrlString);
+                    return NetworkUtils.getResponseFromHttpUrl(getMovieUrl);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String getMoviesResults) {
+        mLoadingIndicator.setVisibility(View.INVISIBLE);
+
+        if (getMoviesResults != null && !getMoviesResults.equals("")) {
+            try {
+                JSONObject response = new JSONObject(getMoviesResults);
+                JSONArray results = response.getJSONArray(Constants.RESULT_KEY);
+                mAdapter.clearItems();
+                for (int i = 0; i < results.length(); i++) {
+                    mAdapter.addPosterItem(new Poster(results.getJSONObject(i)));
+                }
+                mAdapter.notifyDataSetChanged();
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
         }
     }
 
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
